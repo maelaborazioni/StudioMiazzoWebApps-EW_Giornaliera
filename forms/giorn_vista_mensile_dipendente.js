3b765a51ac97373360512422ffe1dc4ff617b140
Giovanni Tizzoni <giovanni.tizzoni@gmail.com>
2019-02-25 12:12:34 +0100

    [ADD] Visualizzazione vista mensile per il dipendente

---
+++ forms/giorn_vista_mensile_dipendente.js
@@ -0,0 +1,20 @@
+
+/**
+ * Callback method for when form is shown.
+ *
+ * @param {Boolean} firstShow form is shown first time after load
+ * @param {JSEvent} event the event that triggered the action
+ *
+ * @private
+ * @override
+ *
+ * @properties={typeid:24,uuid:"7A2E7109-E373-4B5C-8DA1-2B54B499E527"}
+ */
+function onShow(firstShow, event) 
+{
+	globals.objGiornParams[forms.svy_nav_fr_openTabs.vTabNames[forms.svy_nav_fr_openTabs.vSelectedTab]].selected_tab = 2;
+	
+	vSoloDipendente = true;
+	
+	_super.onShow(firstShow, event)
+}
